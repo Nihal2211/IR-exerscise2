e6320200d1ab9ecb84dd11edbad90946f2f38331
Nihal Fathima <nihalf@qatar.cmu.edu>
2015-09-19 02:22:46 +0300

    without arraylist print

--- Index3file.java
+++ Index3file.java
@@ -1,17 +1,107 @@
+
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.io.DataOutputStream;
 
 public class Index3file {
+	
+	static TreeMap <String, HashMap <Integer, ArrayList>> invertedIndex = new TreeMap<String,HashMap <Integer, ArrayList>>();
+	
+  private static TreeMap <String, HashMap <Integer, ArrayList>> 
+  indexing(int docno, String doc) {
+
+			
+			//System.out.println(doc);
+			doc = doc.toLowerCase();
+			String[] line = doc.split("\n");
+			//System.out.println("yeah" + line.length);
+			
+			int positionNumber=0;
+			HashMap <String, ArrayList> docMap = new HashMap <String, ArrayList>();
+			//DocMap is the local index
+			for (int a =0; a< line.length; a++) {            
+				// process the line.
+				if (line[a] == null) {break;}
+				line[a]=line[a].trim();
+				String[] wordlist = line[a].split(" ");	
+				//System.out.println(wordlist[0] + "This is first word");
+				for ( int i =0 ; i <wordlist.length; i ++) {
+					positionNumber +=1;
+					ArrayList <Integer> positionArray = new ArrayList <Integer>();
+					boolean isObjectKeyInHashMap = docMap.containsKey(wordlist[i]);
+					
+					if (wordlist[i].endsWith(".") ){
+						wordlist[i]= wordlist[i].substring(0, wordlist[i].length() - 1);         
+					}
+					if(wordlist[i].matches("[a-zA-Z]*$") && !wordlist[i].matches("^\\s*$")){ 
+						
+						if (isObjectKeyInHashMap == false ) {	
+							positionArray.add(positionNumber);
+							docMap.put(wordlist[i],positionArray);
+						}
+						else {
+							positionArray = docMap.get(wordlist[i]);
+							positionArray.add(positionNumber);
+						}
+					}  
+				}
+			} 
+
+			//merge lineMap (localindex) and global index
+			
+			for (String index : docMap.keySet())
+			{
+				//System.out.println("Index keys" + index);
+				ArrayList <Integer> temp = new ArrayList <Integer>();
+				HashMap <Integer, ArrayList> lineMapinInverted =  new HashMap <Integer, ArrayList>();
+				temp = 	docMap.get(index);
+				if (invertedIndex.containsKey(index)){ 
+					lineMapinInverted = invertedIndex.get(index);
+					lineMapinInverted.put(docno, temp);
+
+				}
+				else {
+					temp=docMap.get(index);
+					lineMapinInverted.put(docno, temp);
+					invertedIndex.put(index, lineMapinInverted);
+				}
+
+			}
+		
+			return invertedIndex; 
+  }	
+	
    public static void main(String[] args) throws IOException {
       
-      InputStream is = null;
-      DataInputStream dis = null;
       
-      try{
+      ArrayList<String> docArray = new ArrayList();
+      ArrayList<String> words = new ArrayList<String>();
+      File directory = new File("C:/Users/syedkhadri/workspace2/Trail/src/Wordfrequency");
+		for (final File file : directory.listFiles()) {
+			System.out.println(file);
+			docArray.add("C:/Users/syedkhadri/workspace2/Trail/src/Wordfrequency/" + file.getName());
+		}
+		
+		
+		InputStream is = null;
+	      DataInputStream dis = null;
+	      FileOutputStream fos = null;
+	      DataOutputStream dos = null;
+			try{
+				
+				for (int docno = 0; docno <docArray.size(); docno++ ){
+					
          // create file input stream
-         is = new FileInputStream("C:/Users/syedkhadri/workspace2/Trail/src/Wordfrequency");
+         is = new FileInputStream(docArray.get(docno));
          
          // create new data input stream
          dis = new DataInputStream(is);
@@ -26,14 +116,42 @@ public class Index3file {
          dis.readFully(buf);
          
          // for each byte in the buffer
-         for (byte b:buf)
+        /* for (byte b:buf)
          {
             // convert byte to char
-            char c = (char)b; 
             
+           
             // prints character
-            System.out.print(c);
-         }
+            System.out.println(b);
+         }*/
+         //String c = buf.toString();
+         String c = new String(buf);
+         indexing(docno +1,c);
+         
+         
+         //words.add(c);
+	   }
+				System.out.println(invertedIndex);
+         fos = new FileOutputStream("C:/Users/syedkhadri/desktop/word1.txt");
+         
+         // create data output stream
+         dos = new DataOutputStream(fos);
+           
+         // for each  string in string buffer
+         for(Entry<String, HashMap<Integer, ArrayList>> entry: invertedIndex.entrySet()) {
+        	    dos.writeUTF(entry.getKey());
+        	    for(Entry<Integer, ArrayList> entry2: entry.getValue().entrySet()) {
+        	    	dos.writeInt(entry2.getKey());
+        	    	
+        	    	 //for(ArrayList<Integer> entry3: entry2.getValue()) {
+        	    	//	 dos.writeInt(entry3);}
+        	    }
+        	    
+        	}
+         // force data to the underlying file output stream
+         dos.flush();
+         
+       
       }catch(Exception e){
          // if any error occurs
          e.printStackTrace();
@@ -45,5 +163,6 @@ public class Index3file {
          if(dis!=null)
             dis.close();
       }
-   }
-}
+			}
+		
+   }	
